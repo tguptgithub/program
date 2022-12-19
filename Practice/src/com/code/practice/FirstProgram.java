b5ecaa2ae10c963a7e8d20038b0cc68dd1a03ad1
admin <admin@DESKTOP-M8JPQI2>
2022-12-19 20:54:32 +0530

    First Program

---
+++ Practice/src/com/code/practice/FirstProgram.java
@@ -0,0 +1,16 @@
+package com.code.practice;
+
+public class FirstProgram {
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	
+
+	}
+
+}
